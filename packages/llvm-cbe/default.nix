1b2299e14a33eb72a85adfca50fc29f865fa9bcf
LimeTea <fr3shtea@outlook.com>
2022-04-10 03:19:34 +0100

    Add LLVM-CBE v3

--- packages/llvm-cbe/default.nix
+++ packages/llvm-cbe/default.nix
@@ -9,5 +9,7 @@ stdenv.mkDerivation {
   src = fetchFromGitHub {
     repo = "llvm-cbe";
     owner = "JuliaComputingOSS";
+    rev = "bc21b898326787e0dcc0e7fe0a4d001e44f9fcd3";
+    sha256 = "sha256-DPovfhjVMG/TdnhE5cVgI/33KHnWYgtQTNAYiyw2O+c=";
   };
 }
