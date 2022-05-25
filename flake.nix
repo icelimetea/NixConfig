bdc28e26560711448015c0862b113ee077978510
icelimetea <fr3shtea@outlook.com>
2022-05-25 22:07:20 +0100

    Bump nixpkgs

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable-small";
+    nixpkgs.url = "github:NixOS/nixpkgs/882fc91780e0b2744f2eff19cc644693f9adab11";
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
