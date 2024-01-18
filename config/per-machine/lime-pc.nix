ca58a42755ae2abc1a78942a5ab7ba488ca1b60e
icelimetea <fr3shtea@outlook.com>
2024-01-19 00:28:51 +0300

    Add Nvidia drivers

--- config/per-machine/lime-pc.nix
+++ config/per-machine/lime-pc.nix
@@ -14,7 +14,20 @@
     opengl.extraPackages = [ pkgs.intel-media-driver ];
 
     cpu.intel.updateMicrocode = true;
+
+    nvidia = {
+      modesetting.enable = true;
+
+      prime = {
+        sync.enable = true;
+
+        intelBusId = "PCI:0:2:0";
+	nvidiaBusId = "PCI:1:0:0";
+      };
+    };
   };
+
+  services.xserver.videoDrivers = [ "nvidia" ];
 
   boot.initrd.luks.devices.vol1.device = "/dev/disk/by-uuid/fdac2ff7-7355-4c02-aefc-119d81e04aba";
   boot.initrd.luks.devices.vol2.device = "/dev/disk/by-uuid/5f4d07d1-18ac-4b32-b049-602a8813a288";
