5f2e07459931c614d4da81cd456af5733d348dcd
icelimetea <fr3shtea@outlook.com>
2024-01-21 18:08:30 +0300

    Fix compilation

--- config/dwm/config.h
+++ config/dwm/config.h
@@ -18,7 +18,7 @@ static const char *colors[][3]      = {
 /* tagging */
 static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
 
-static const Rule rules[] = {
+static const Rule rules[0] = {
 	/* xprop(1):
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
