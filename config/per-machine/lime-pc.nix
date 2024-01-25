92dfa8564dcef2647167fdc9c82e72a825df6576
icelimetea <fr3shtea@outlook.com>
2024-01-26 00:52:09 +0300

    Enable NVIDIA render offloading

--- config/per-machine/lime-pc.nix
+++ config/per-machine/lime-pc.nix
@@ -20,7 +20,10 @@
       powerManagement.enable = true;
 
       prime = {
-        sync.enable = true;
+        offload = {
+	  enable = true;
+	  enableOffloadCmd = true;
+        };
 
         intelBusId = "PCI:0:2:0";
 	nvidiaBusId = "PCI:1:0:0";
