85bd3681e6334681c5a261d0bf5336c9aee0a7ec
icelimetea <fr3shtea@outlook.com>
2024-02-27 17:24:05 +0300

    Let's try nouveau!

--- config/per-machine/lime-pc.nix
+++ config/per-machine/lime-pc.nix
@@ -7,30 +7,16 @@
 
   boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "usb_storage" "sd_mod" ];
   boot.initrd.kernelModules = [ "dm-snapshot" ];
-  boot.kernelModules = [ "kvm-intel" ];
+  boot.kernelModules = [ "kvm-intel" "nouveau" ];
   boot.extraModulePackages = [ ];
 
   hardware = {
     opengl.extraPackages = [ pkgs.intel-media-driver ];
 
     cpu.intel.updateMicrocode = true;
-
-    nvidia = {
-      modesetting.enable = true;
-
-      prime = {
-        offload = {
-          enable = true;
-          enableOffloadCmd = true;
-        };
-
-        intelBusId = "PCI:0:2:0";
-        nvidiaBusId = "PCI:1:0:0";
-      };
-    };
   };
 
-  services.xserver.videoDrivers = [ "nvidia" ];
+  services.xserver.videoDrivers = [ "nouveau" ];
 
   boot.initrd.luks.devices.vol1.device = "/dev/disk/by-uuid/fdac2ff7-7355-4c02-aefc-119d81e04aba";
   boot.initrd.luks.devices.vol2.device = "/dev/disk/by-uuid/5f4d07d1-18ac-4b32-b049-602a8813a288";
