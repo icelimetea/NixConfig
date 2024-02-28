603fa2ba25c080c67ab6fe515de1bdc53e2647e9
icelimetea <fr3shtea@outlook.com>
2024-02-28 07:18:32 +0000

    Revert nouveau

--- config/per-machine/lime-pc.nix
+++ config/per-machine/lime-pc.nix
@@ -6,7 +6,7 @@
     ];
 
   boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "usb_storage" "sd_mod" ];
-  boot.initrd.kernelModules = [ "dm-snapshot" "nouveau" ];
+  boot.initrd.kernelModules = [ "dm-snapshot" ];
   boot.kernelModules = [ "kvm-intel" ];
   boot.extraModulePackages = [ ];
 
@@ -15,8 +15,6 @@
 
     cpu.intel.updateMicrocode = true;
   };
-
-  services.xserver.videoDrivers = [ "nouveau" ];
 
   boot.initrd.luks.devices.vol1.device = "/dev/disk/by-uuid/f7dc00c4-8dc8-43d7-bf92-9ea60aec5f84";
   boot.initrd.luks.devices.vol2.device = "/dev/disk/by-uuid/50048bb3-3c3b-442a-9aee-9c9ff3755e76";
