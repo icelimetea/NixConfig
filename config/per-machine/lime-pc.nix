97efee6e90de8dfee13d2abf6bee5c3ae3fde702
icelimetea <fr3shtea@outlook.com>
2024-04-18 21:12:48 +0300

    Nouveau: second try

--- config/per-machine/lime-pc.nix
+++ config/per-machine/lime-pc.nix
@@ -5,7 +5,7 @@
     [ (modulesPath + "/installer/scan/not-detected.nix")
     ];
 
-  boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "usb_storage" "sd_mod" ];
+  boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "usb_storage" "sd_mod" "nouveau" ];
   boot.initrd.kernelModules = [ "dm-snapshot" ];
   boot.kernelModules = [ "kvm-intel" ];
   boot.extraModulePackages = [ ];
@@ -14,23 +14,7 @@
     opengl.extraPackages = [ pkgs.intel-media-driver ];
 
     cpu.intel.updateMicrocode = true;
-
-    nvidia = {
-      modesetting.enable = true;
-
-      prime = {
-        offload = {
-          enable = true;
-          enableOffloadCmd = true;
-        };
-
-        intelBusId = "PCI:0:2:0";
-        nvidiaBusId = "PCI:1:0:0";
-      };
-    };
   };
-
-  services.xserver.videoDrivers = [ "nvidia" ];
 
   boot.initrd.luks.devices.vol1.device = "/dev/disk/by-uuid/f7dc00c4-8dc8-43d7-bf92-9ea60aec5f84";
   boot.initrd.luks.devices.vol2.device = "/dev/disk/by-uuid/50048bb3-3c3b-442a-9aee-9c9ff3755e76";
